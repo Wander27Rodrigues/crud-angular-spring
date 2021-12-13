4e139903b9aed01645fd9b4deb5c6ec69b21e02e
wander rodrigues <wanderrodrigues_@outlook.com>
2021-12-12 22:26:47 -0300

    add spinner

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,30 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
 
-
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
-
-  courses: Course[] = [
-    { _id: '1', name: 'Angular', category: 'front-end' }
-  ];
-  displayedColumns = [ 'name', 'category' ];
+  courses$: Observable<Course[]>;
+  displayedColumns = ['name', 'category'];
 
   //CoursesService: CoursesService;
 
   constructor(private CoursesService: CoursesService) {
     //this.courses = [];
     //this.CoursesService = new CoursesService();
-
-  }
-  ngOnInit(): void {
-    this.courses = this.CoursesService.list();
+    this.courses$ = this.CoursesService.list();
   }
 
+  ngOnInit(): void {}
 }
