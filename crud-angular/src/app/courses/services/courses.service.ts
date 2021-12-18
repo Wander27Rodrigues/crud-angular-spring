b55cae408c3c5f22ff95919c177bee9887498cc2
wander rodrigues <wanderrodrigues_@outlook.com>
2021-12-18 11:29:40 -0300

    crud-spring-api

---
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { delay, first, tap } from 'rxjs';
+
+import { Course } from '../model/course';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CoursesService {
+
+  private readonly API = '/assets/courses.json';
+
+  constructor(private httpClient: HttpClient) { }
+
+  // Observable que retorna um array de cursos
+  list() {
+    return this.httpClient.get<Course[]>(this.API)
+      .pipe(
+      first(),
+      delay(4000),
+      tap(courses => console.log(courses))
+    );
+  }
+
+}
