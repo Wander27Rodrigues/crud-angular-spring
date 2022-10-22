3e66e99b7a3d2b535b1b59bb93f08dc6957b280c
wander souza <wanderrodrigues_@outlook.com>
2022-10-22 05:43:35 -0300

    feat(build): ajuste api

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -14,7 +15,8 @@ export class CourseFormComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder,
     private service: CoursesService,
-    private snackBar: MatSnackBar) { 
+    private snackBar: MatSnackBar,
+    private location: Location) { 
     this.form = this.formBuilder.group({
       name: [null],
       category: [null]
@@ -28,13 +30,20 @@ export class CourseFormComponent implements OnInit {
     console.log('Formulario enviado' + this.form.value);
 
     this.service.save(this.form.value)
-    .subscribe(data => console.log(data), error => {
-      this.snackBar.open('Error ao salvar curo.', action: '', duration);
-    });
+    .subscribe(data => this.onSucces(), error => this.onError());
   }
 
   onCancel(){
-    
+    this.location.back();
+  }
+
+   private onSucces() {
+    this.snackBar.open('Curso salvo com sucesso!','', { duration: 5000 });
+    this.onCancel();
+   }
+
+  private onError(){
+    this.snackBar.open('Error ao salvar curo.','', { duration: 5000 });
   }
 
 }
