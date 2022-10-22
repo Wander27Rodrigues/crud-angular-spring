e32d8df503076c7181a8d6890cba1fa444b0f959
wander souza <wanderrodrigues_@outlook.com>
2022-10-22 06:10:01 -0300

    Fix: corrção de test

--- api/src/test/java/com/crudspring/CrudApplicationTests.java
+++ api/src/test/java/com/crudspring/CrudApplicationTests.java
@@ -1,9 +1,10 @@
 package com.crudspring;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+
+@ContextConfiguration
 class CrudApplicationTests {
 
 	@Test
